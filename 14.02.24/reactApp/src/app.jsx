4ab76133f3a8d97e89efa5ce62b9ac53aaee3430
kalpeshdhotre <kalpeshdhotre@hotmail.com>
2024-02-14 09:32:42 +0530

    first practive ract app

---
+++ 14.02.24/reactApp/src/app.jsx
@@ -0,0 +1,30 @@
+import { useState } from "preact/hooks";
+import preactLogo from "./assets/preact.svg";
+import viteLogo from "/vite.svg";
+import "./app.css";
+
+export function App() {
+   const [count, setCount] = useState(0);
+
+   return (
+      <>
+         <h1>Counter is: {count} </h1>
+         <button
+            onClick={() => {
+               if (count < 5) {
+                  setCount(count + 1);
+               }
+            }}
+         >
+            Add to Count
+         </button>
+         <button
+            onClick={() => {
+               if (count != -5) setCount(count - 1);
+            }}
+         >
+            Deduct from Count
+         </button>
+      </>
+   );
+}
