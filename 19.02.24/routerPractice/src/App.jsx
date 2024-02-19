c91a5c541f3d374f7d195da2d3e291d8e703adc0
kalpeshdhotre <kalpeshdhotre@hotmail.com>
2024-02-19 19:57:14 +0530

    Lazy Loading and suspense understood

--- 19.02.24/routerPractice/src/App.jsx
+++ 19.02.24/routerPractice/src/App.jsx
@@ -1,20 +1,52 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from "react";
+import { Suspense, lazy } from "react";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+
 import NavBar from "./components/NavBar";
-import Dashboard from "../../week7Practice/src/components/Dashboard";
-import Landing from "../../week7Practice/src/components/Landing";
+// import Dashboard from "./components/Dashboard";
+// import Landing from "./components/Landing";
+
+// const NavBar = lazy(() => "./components/NavBar");
+const Dashboard = lazy(() => import("./components/Dashboard"));
+const Landing = lazy(() => import("./components/Landing"));
 
 function App() {
    return (
-      <>
+      <div>
          <BrowserRouter>
             <NavBar />
+
             <Routes>
-               <Route path="/" element={<Landing />} />
-               <Route path="/Dashboard" element={<Dashboard />} />
+               <Route
+                  path="/Dashboard"
+                  element={
+                     <Suspense>
+                        <Dashboard />
+                     </Suspense>
+                  }
+               />
+               <Route
+                  path="/"
+                  element={
+                     <Suspense>
+                        <Landing />
+                     </Suspense>
+                  }
+               />
             </Routes>
          </BrowserRouter>
-      </>
+      </div>
    );
 }
+
+// function NavBar() {
+//    const navigate = useNavigate();
+//    return (
+//       <div>
+//          <button onClick={() => navigate("/")}>HomePage</button>
+//          <button onClick={() => navigate("/Dashboard")}>Dashboard</button>
+//       </div>
+//    );
+// }
 
 export default App;
